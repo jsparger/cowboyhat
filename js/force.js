0f48951b2741c6cad3cb78954219c2e9891a2862
John Sparger <jsparger87@gmail.com>
2017-12-01 00:07:32 +0100

    fixed a lot of problems with how I was doing the force layout.

--- js/force.js
+++ js/force.js
@@ -28,6 +28,8 @@ define(["d3"], function (d3) {
         .attr("width", this._width)
         .attr("height", this._height);
 
+      this._nodes = this._svg.append("g").selectAll(".node");
+
       // create a border for the svg.
       this._svg.append("rect")
        			.attr("x", 0)
@@ -39,15 +41,19 @@ define(["d3"], function (d3) {
        			.style("stroke-width", 5);
 
       // fetch first data from source:
-      this._data = [await this._fetch(this._root_key)];
+      //this._data = [await this._fetch(this._root_key)];
       // for example with a few balls
-      // this._data = [{name: "steve"}, {name: "joe"}, {name: "robert"}];
+      this._data = [{name: "steve"}, {name: "joe"}, {name: "robert"}];
 
       // initialize the simulation
-      this._sim = d3.forceSimulation(this._data)
-          .force("charge", d3.forceManyBody())
+      this._sim = d3.forceSimulation()
+          .force("charge", d3.forceManyBody().strength(-500))
+          // .force("gravity", d3.forceManyBody().strength(100))
           // .force("link", d3.forceLink(links))
-          .force("center", d3.forceCenter(this._width/2,this._height/2))
+          .force("forceX", d3.forceX(this._width/2))
+          .force("forceY", d3.forceY(this._height/2))
+          // .force("center", d3.forceCenter(this._width/2, this._height/2))
+          // .force("radial", d3.forceRadial(100, this._width/2, this._height/2))
           .on("tick", this.tick.bind(this));
 
       // run the simulation and visualization
@@ -55,32 +61,35 @@ define(["d3"], function (d3) {
     }
 
     update() {
-      this._sim.restart();
+      // To understand this code, check out this explanation of d3.js selections.
+      // detailed:   https://bost.ocks.org/mike/selection/
+      // simplified: https://bost.ocks.org/mike/join/
 
-      // compare the content of the svg group "nodes" with our data
-      // nodes represents the elements which were in both the svg and our data.
-      let nodes = this._svg.selectAll("node")
-        .data(this._data);
+      // Join our new _data with the old data in the selection (initially none)
+      // "_nodes" now represents nodes which existed both our new and old data.
+      this._nodes = this._nodes.data(this._data);
 
-      // remove any nodes which were in the svg but not in our data.
-      // FYI this is called the "exit" group
-      nodes.exit().remove();
+      // remove any nodes which associated with data we deleted (the exit group)
+      this._nodes.exit().remove();
 
-      // for every node in our data which was not in the svg (the "enter" group)
-      // create a circle. Then merge this "enter" group with nodes. Finally,
-      // update the positions of every svg element based on their node.
-      nodes.enter().append("circle")
+      // for every new piece of data, (the "enter" group), create a circle in
+      // the svg group. Then, merge the "enter" group with _nodes. Now _nodes
+      // is a selection with one node per piece of element in _data.
+      this._nodes = this._nodes.enter()
+          .append("circle")
           .attr("r", 10)
-        .merge(nodes)
-          .attr("cx", function(d) { return d.x; })
-          .attr("cy", function(d) { return d.y; });
+        .merge(this._nodes);
+
+      this._sim.nodes(this._data);
+      this._sim.alpha(5).restart();
     }
 
     tick() {
-      this._svg.selectAll("circle")
-      .attr("transform", function(d) {
-      		return "translate(" + d.x + "," + d.y + ")";
-        });
+      // update the positions of the nodes in the svg based on their coordinates
+      // as computed by the force layout.
+      this._nodes
+        .attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; });
     }
   }
 
